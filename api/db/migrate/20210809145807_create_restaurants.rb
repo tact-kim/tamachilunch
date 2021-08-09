3269cce99438f5f3cc584d9264740358cc20a2c8
tact-kim <takuto.kimura.631604@gmail.com>
2021-08-10 00:07:30 +0900

    コントローラー・モデル・seed追加

---
+++ api/db/migrate/20210809145807_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :genre
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!(
+  [
+    {name: "龍祥軒", genre: "中華", address: "東京都港区芝浦3-6-8うつみビル1F"},
+    {name: '武蔵', genre: 'ラーメン', address: '東京都港区芝浦3丁目12-5'},
+    {name: 'はんぞう', genre: '和食', address: '東京都港区芝浦3丁目6-8TOKIビルB1'},
+    {name: '正直や', genre: '和食', address: '東京都港区芝浦3-13-9藤ビルB1'},
+  ]
+)
